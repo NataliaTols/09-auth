e991e07277eef01b02531d9b09d6b16e566c0911
NataliaTols <kopylova9624@gmail.com>
2026-04-07 21:48:10 +0200

    rename file

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import css from "./NoteForm.module.css";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createNote } from "../../lib/api";
-import { useNoteDraftStore } from '@/lib/stores/noteStore';
+import { useNoteDraftStore } from '@/lib/store/noteStore';
 
 type TagType = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 
